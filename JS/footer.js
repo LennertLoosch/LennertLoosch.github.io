19255d26e8692ae9f2796b0cee99d3a877f479dd
Wamidz <lennertloosch@outlook.com>
2025-05-27 11:51:33 +0200

    changes

--- JS/footer.js
+++ JS/footer.js
@@ -1,11 +1,7 @@
-// Get the current year
 const currentYear = new Date().getFullYear();
 
-// Get the footer element
 const footer = document.querySelector('footer');
 
-// Create the copyright text
 const copyrightText = `&copy; ${currentYear} All Rights Reserved for the Content on This Website.`;
 
-// Set the innerHTML of the footer to the copyright text
 footer.innerHTML = copyrightText;
